e0fc6736b392349f834c0fdb76b24d0875cc1767
Sven <sven@mutzl.org>
2018-06-16 16:10:27 +0200

    Add option to set custom url for mongo db

--- provider/libraries/mongo.js
+++ provider/libraries/mongo.js
@@ -1,5 +1,5 @@
 let MongoClient = require('mongodb').MongoClient,
-	mongo_config = require('../config/mongo.js');
+    mongo_config = require('../config/mongo.js');
 
 var _db;
 
@@ -8,18 +8,18 @@ module.exports = {
 };
 
 function connect() {
-	return new Promise((resolve, reject) => {
-		if (_db !== undefined)
-			resolve(_db);
-		else {
-			let url = 'mongodb://' + mongo_config.host + ':' + mongo_config.port + '/' + mongo_config.database;
-			MongoClient.connect(url, function(err, db) {
-				if (err) throw Error(err.message);
-				else {
-					resolve(db);
-					_db = db;
-				}
-			});
-		}
-	});
+    return new Promise((resolve, reject) => {
+        if (_db !== undefined)
+            resolve(_db);
+        else {
+            let url = (mongo_config.url) ? mongo_config.url : 'mongodb://' + mongo_config.host + ':' + mongo_config.port + '/' + mongo_config.database;
+            MongoClient.connect(url, function(err, db) {
+                if (err) throw Error(err.message);
+                else {
+                    resolve(db);
+                    _db = db;
+                }
+            });
+        }
+    });
 }
